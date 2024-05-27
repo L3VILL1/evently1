5859105fed51981b398c286cab2c812ccf6cac3d
L3VILL1 <levi_dantasgomes@hotmail.com>
2024-05-27 07:37:12 -0300

    first git

---
+++ app/(root)/page.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cottain py-5 md:py-10">
+        <div className="wrapper grid grid-col-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Hospede seus eventos na nossa plataforma!</h1>
+            <p className="p-regular-20 md:p-regular-24">Hospede e se inscreva em mais de 3,000 eventos dos melhores eventos do mundo!</p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">
+                Explore agora
+              </Link>
+            </Button>
+          </div>
+
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[7-vh] object-cotain object-center 2
+          2xl:max-h-[50vh]"
+          />
+        </div>
+      </section>
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">
+          Confiado <br /> por milhares de eventos
+        </h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          
+        </div>
+      </section>
+
+    </>
+  );
+}
